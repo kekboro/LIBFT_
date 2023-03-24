24816c2ea0f4c5a893b6056c230a48a88162209a
Nero Van Gestel <nero@HUAWEI_P8_lite_2017-bb61b.demarkten.priavte>
2023-03-24 16:46:42 +0100

    1

--- srcs2/ft_striteri____.c
+++ srcs2/ft_striteri____.c
@@ -1,6 +1,20 @@
 #include "libft.h"
 
-void ft_striteri(char *s, void (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	
+	int		i;
+	int		i2;
+
+	i = 0;
+	i2 = 0;
+	if (s != NULL && f != NULL)
+	{
+		i = ft_strlen(s);
+		while (i2 < i)
+		{
+			(*f)(i2, s);
+			s++;
+			i2++;
+		}
+	}
 }
