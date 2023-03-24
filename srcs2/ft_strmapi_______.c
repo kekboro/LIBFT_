19be0d91eadd0790e3e2fd42687d3b8bce0a6ccb
Nero Van Gestel <nero@HUAWEI_P8_lite_2017-bb61b.demarkten.priavte>
2023-03-24 16:20:49 +0100

    1

---
+++ srcs2/ft_strmapi_______.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char			*result;
+
+	i = 0;
+	if (s == NULL || f == NULL)
+		return (NULL);
+	result = ft_memalloc(ft_strlen(s));
+	ft_strcpy(result, s);
+	while (result[i])
+	{
+		result[i] = (*f)(i, result[i]);
+		i++;
+	}
+	return (result);
+}
